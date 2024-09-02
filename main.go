728d8e04763e9502871cffd6832ea1171072e538
Dheeraj Singh Jodha <djodha@redhat.com>
2024-09-02 06:42:38 +0530

    test: add jwt-go dep containin vulnerability
    * this dep doesn't have a known fix yet.
    * so clair-scan will flag it as a warning
    * and pass the overall task

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
 func main() {
 	router := setupRouter()
 	router.Run(":8080")
+
+	// Example usage of jwt-go to make it a direct dependency
+	token := jwt.New(jwt.SigningMethodHS256)
+	fmt.Println(token)
 }
